f753eccdf346ec4447d52a30c0f807b53732f34a
axieax <x.and.i.rew.e@gmail.com>
2021-04-02 18:59:36 +1100

    completed server functions - note untested yet

---
+++ listener.py
@@ -0,0 +1,59 @@
+import jwt
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from secrets import token_urlsafe
+
+JWT_ALGORITHM = 'HS256'
+
+data = {
+    'sessions': [],
+    'private_key': b'',
+    'public_key': b'',
+    'jwt_secret': b'',
+}
+
+
+def initialise_listener() -> None:
+    # Generate RSA pairs
+    key = RSA.generate(bits=2048)
+    # generate private key
+    private_key = key.export_key()
+    data['private_key'] = private_key
+    # generate public key
+    public_key = key.publickey().export_key()
+    data['public_key'] = public_key
+    # generate jwt secret token
+    data['jwt_secret'] = token_urlsafe()
+
+def get_public_key() -> bytes:
+    return data['public_key']
+
+def decrypt_data(session_id: str, browser_name: str, encrypted_data: str) -> None:
+    if not valid_session(session_id):
+        return
+
+    # decrypt data
+    private_key = data['private_key']
+    cipher = PKCS1_OAEP.new(private_key)
+    decrypted_data = cipher.decrypt(encrypted_data)
+    # decide data
+    decoded = jwt.decode(decrypted_data, data['secret_key'], algorithms=[JWT_ALGORITHM])
+    print(browser_name)
+    print(decoded)
+
+def new_session() -> str:
+    session_id = token_urlsafe()
+    data['sessions'].append(session_id)
+    return session_id
+
+def valid_session(session_id: str) -> bool:
+    return session_id in data['sessions']
+
+def delete_session(session_id: str) -> None:
+    if valid_session(session_id):
+        data['sessions'].remove(session_id)
+
+
+initialise_listener()
+print(data)
+
---
+++ server.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from threading import Thread
+from listener import initialise_listener, new_session, get_public_key, decrypt_data, delete_session
+
+APP = Flask(__name__)
+
+@APP.route('/', methods=['GET', 'POST', 'DELETE'])
+def index():
+    if request.method == 'GET':
+        session_id = new_session()
+        return {
+            'public_key': get_public_key,
+            'session_id': session_id,
+        }
+    elif request.method == 'POST':
+        payload = request.json()
+        decrypt_data(payload['session_id'], payload['browser_name'], payload['data'])
+        return {}
+    else:
+        session_id = request.args.get('session_id')
+        delete_session(session_id)
+        return {}
+
+def run():
+    APP.run(host='0.0.0.0')
+
+def start_server():
+    t = Thread(target=run)
+    t.start()
+
+if __name__ == '__main__':
+    initialise_listener()
+    start_server()
